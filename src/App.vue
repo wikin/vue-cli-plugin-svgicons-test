8d04c9261deafc0d261f1913b71ae409db8bca83
wikin <wiki@atlas.cz>
2021-11-06 15:53:32 +0100

    update to v0.0.3

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,11 @@
 <template>
   <img alt="Vue logo" src="./assets/logo.png">
-  <icon-check :size=200 :stroke=5 color="green" />
+  <ul class="icons">
+    <li><icon-check /></li>
+    <li><icon-check :stroke=8 /></li>
+    <li><icon-check :size=128 :stroke=0.2 /></li>
+    <li><icon-check :size=64 :stroke=6 color="#41b883" /></li>
+  </ul>
   <HelloWorld msg="Welcome to Your Vue.js App"/>
 </template>
 
@@ -23,5 +28,22 @@ export default {
   text-align: center;
   color: #2c3e50;
   margin-top: 60px;
+}
+ul.icons {
+  list-style: none;
+  margin: 0;
+  padding: 0;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+}
+ul.icons li {
+  background: #e5e5e5;
+  width: 150px;
+  height: 150px;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  margin: 2px;
 }
 </style>
---
+++ vue.config.js
@@ -0,0 +1,7 @@
+module.exports = {
+  pluginOptions: {
+    svgicons: {
+      stroke: 1
+    }
+  }
+}
